b9d12fb5ba91221e8b1a94e9008c1dba0e7b1168
Conrad Groenewald <conradnoah@Conrads-MacBook-Air.local>
2016-10-10 00:59:12 +0100

    added some styling

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -2,20 +2,28 @@ import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 
-	@Injectable()
+@Injectable()
 export class GithubService{
-	private username:string;
-	private client_is = '9f3c71dfd09c1648905d';
-	private client_secret = 'd298f4dea28aa7115a9b43f17e9b2a907938a97a';
-	
-constructor(private _http: Http){
-	console.log('Service ready....');
-	this.username = 'conradgroenewalddev';
-	}
-	
-getUser(){
-	return this._http.get('http://api.github.com/users/' + this.username + '?client_id='
-		+ this.client_id + '&client_secret=' + this.client_secret)
-		.map(res => res.json());
-}
+    private username:string;
+    private client_id = '9f3c71dfd09c1648905d';
+    private client_secret = 'd298f4dea28aa7115a9b43f17e9b2a907938a97a';
+    
+    constructor(private _http: Http){
+        console.log('Github Service Ready...');
+        this.username = 'bradtraversy';
+    }
+    
+    getUser(){
+        return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    getRepos(){
+        return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+            .map(res => res.json());
+    }
+    
+    updateUser(username:string){
+        this.username = username;
+    }
 }
